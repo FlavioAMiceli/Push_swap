40cddfba170f68e5fcbea441d983118a49cc9511
Flavio Miceli <flavio.armando.miceli@gmail.com>
2020-05-12 15:14:26 +0200

    Fix: better bound and pivot calculation

--- sift.c
+++ sift.c
@@ -19,9 +19,16 @@ static void	sift_to_b(t_stacks *s, int pivot, int len)
 
 	pushed = 0;
 	rotated = 0;
+	ft_putchar('\n');
+	ft_putendl("sift_to_b");
+	ft_putstr("Part len: ");
+	ft_putnbr(len);
+	ft_putstr(" Pivot : ");
+	ft_putnbr(pivot);
+	ft_putchar('\n');
 	while (pushed < len)
 	{
-		if (stack_get(s->a, 0) < pivot)
+		if (stack_get(s->a, 0) <= pivot)
 		{
 			push(&(s->a), &(s->b));
 			ft_putendl("pb");
@@ -32,11 +39,14 @@ static void	sift_to_b(t_stacks *s, int pivot, int len)
 		ft_putendl("ra");
 		rotated++;
 	}
-	while (rotated != s->a->len && rotated > 0)
+	if (rotated != s->a->len)
 	{
-		rev_rot(&(s->a));
-		ft_putendl("rra");
-		rotated--;
+		while (rotated > 0)
+		{
+			rev_rot(&(s->a));
+			ft_putendl("rra");
+			rotated--;
+		}
 	}
 }
 
@@ -47,9 +57,16 @@ static void	sift_to_a(t_stacks *s, int pivot, int len)
 
 	pushed = 0;
 	rotated = 0;
+	ft_putchar('\n');
+	ft_putendl("sift_to_a");
+	ft_putstr("Part len: ");
+	ft_putnbr(len);
+	ft_putstr(" Pivot : ");
+	ft_putnbr(pivot);
+	ft_putchar('\n');
 	while (pushed < len)
 	{
-		if (stack_get(s->b, 0) >= pivot)
+		if (stack_get(s->b, 0) > pivot)
 		{
 			push(&(s->b), &(s->a));
 			ft_putendl("pa");
@@ -60,11 +77,14 @@ static void	sift_to_a(t_stacks *s, int pivot, int len)
 		ft_putendl("rb");
 		rotated++;
 	}
-	while (rotated != s->b->len && rotated > 0)
+	if (rotated != s->b->len)
 	{
-		rev_rot(&(s->b));
-		ft_putendl("rrb");
-		rotated--;
+		while (rotated > 0)
+		{
+			rev_rot(&(s->b));
+			ft_putendl("rrb");
+			rotated--;
+		}
 	}
 }
 
@@ -74,14 +94,22 @@ static void	partition_to_a(t_stacks *s, int *sorted, int lo, int hi)
 
 	ft_putchar('\n');
 	ft_putendl("part_a");
+	ft_putstr("Part len: ");
+	ft_putnbr(hi - lo + 1);
+	ft_putchar('\n');
 	stack_print(s->a);
 	stack_print(s->b);
 	if ((hi - lo) + 1 <= BASE_CASE_LEN)
 		bc_to_a(s, lo, hi);
 	else
 	{
+		ft_putstr("Hi: ");
+		ft_putnbr(hi);
+		ft_putstr(" Lo: ");
+		ft_putnbr(lo);
+		ft_putchar('\n');
 		pivot_i = (lo + hi) / 2;
-		sift_to_a(s, sorted[pivot_i], (hi - pivot_i) + 1);
+		sift_to_a(s, sorted[pivot_i], hi - pivot_i);
 		partition_to_b(s, sorted, pivot_i + 1, hi);
 		partition_to_a(s, sorted, lo, pivot_i);
 	}
@@ -93,15 +121,23 @@ void		partition_to_b(t_stacks *s, int *sorted, int lo, int hi)
 
 	ft_putchar('\n');
 	ft_putendl("part_b");
+	ft_putstr("Part len: ");
+	ft_putnbr(hi - lo + 1);
+	ft_putchar('\n');
 	stack_print(s->a);
 	stack_print(s->b);
 	if ((hi - lo) + 1 <= BASE_CASE_LEN)
 		bc_to_b(s, lo, hi);
 	else
 	{
-		pivot_i = (lo + hi + 1) / 2;
-		sift_to_b(s, sorted[pivot_i], pivot_i - lo);
-		partition_to_b(s, sorted, pivot_i, hi);
-		partition_to_a(s, sorted, lo, pivot_i - 1);
+		ft_putstr("Hi: ");
+		ft_putnbr(hi);
+		ft_putstr(" Lo: ");
+		ft_putnbr(lo);
+		ft_putchar('\n');
+		pivot_i = (lo + hi) / 2;
+		sift_to_b(s, sorted[pivot_i], pivot_i - lo + 1);
+		partition_to_b(s, sorted, pivot_i + 1, hi);
+		partition_to_a(s, sorted, lo, pivot_i);
 	}
 }
